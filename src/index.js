f98800a3750713f8d2f3091d0279433462b5a478
Sean Poole <spoole5289@gmail.com>
2023-12-12 17:37:21 -0800

    removed unused dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.js';
 import { AuthProvider } from "./context/AuthProvider";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
