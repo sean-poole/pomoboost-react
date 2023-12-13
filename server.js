c5d0f52e206f215fca21aa6e110f6e1995c3526e
Sean Poole <spoole5289@gmail.com>
2023-12-12 21:13:17 -0800

    configure for hosting

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 app.use("/", homeRoutes);
 app.use("/tasks", taskRoutes);
 
-app.use (express.static(path.join(__dirname, ".client/build")));
+app.use(express.static(path.join(__dirname, ".client/build")));
 app.get("*", function(_, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"), function(err) {
     res.status(500).send(err);
